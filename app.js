45c4dbf38e78bb9bbc478503933d672cf776269c
Thakur Saad <thakursaad613@gmail.com>
2022-09-20 21:58:13 +0600

    mock data prototype

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("app is chillin like a villin");
+  res.send("Inventory manager is chillin like a villin");
 });
 
 module.exports = app;
